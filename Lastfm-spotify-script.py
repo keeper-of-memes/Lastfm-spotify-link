2c59f0a75ae0102e745da7645fc5c0a818c04438
keeper-of-memes <118731582+keeper-of-memes@users.noreply.github.com>
2023-12-05 09:41:13 +0000

    Add files via upload

---
+++ Lastfm-spotify-script.py
@@ -0,0 +1,92 @@
+import requests
+from datetime import datetime, timedelta
+import json
+
+SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
+SPOTIFY_SEARCH_URL = 'https://api.spotify.com/v1/search'
+LASTFM_USERNAME = 'LASTFM USERNAME'  # Replace with your Last.fm username
+LASTFM_API_KEY = 'LASTFM API KEY'  # Replace with your Last.fm API key
+
+# Define global variables for Spotify client credentials
+SPOTIFY_CLIENT_ID = 'SPOTIFY API CLIENT ID'  # Replace with your Spotify client ID
+SPOTIFY_CLIENT_SECRET = 'SPOTIFY API SECRET KEY'  # Replace with your Spotify client secret
+
+def get_spotify_access_token(client_id, client_secret):
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
+
+    response = requests.post(SPOTIFY_TOKEN_URL, data=data)
+    
+    if response.status_code == 200:
+        token_data = response.json()
+        access_token = token_data.get('access_token')
+        return access_token
+    else:
+        print(f"Failed to obtain access token. Status code: {response.status_code}")
+        return None
+
+def read_lastfm_track(api_key, username, limit=1):
+    url = f'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={username}&api_key={api_key}&format=json&limit={limit}'
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+
+        if 'track' in data['recenttracks'] and data['recenttracks']['track']:
+            return data['recenttracks']['track'][0]
+        else:
+            print("No recent tracks found.")
+            return None
+    except Exception as e:
+        print(f"Error getting track information from Last.fm: {e}")
+        return None
+
+def search_spotify_track(access_token, track_name, artist_name):
+    headers = {'Authorization': f'Bearer {access_token}'}
+    params = {
+        'q': f'track:{track_name} artist:{artist_name}',
+        'type': 'track'
+    }
+
+    response = requests.get(SPOTIFY_SEARCH_URL, headers=headers, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('tracks', {}).get('items', [])
+    else:
+        print(f"Failed to search track on Spotify. Status code: {response.status_code}")
+        return []
+
+def create_output_dictionary(lastfm_track, spotify_track):
+    output_dict = {
+        'title': lastfm_track.get('name'),
+        'artist': lastfm_track.get('artist', {}).get('#text'),
+        'spotify_link': spotify_track[0]['external_urls']['spotify'] if spotify_track else None
+    }
+    return output_dict
+
+if __name__ == "__main__":
+    access_token = get_spotify_access_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+
+    if access_token:
+        lastfm_track = read_lastfm_track(LASTFM_API_KEY, LASTFM_USERNAME)
+
+        if lastfm_track:
+            track_name = lastfm_track.get('name')
+            artist_name = lastfm_track.get('artist', {}).get('#text')
+
+            if track_name and artist_name:
+                spotify_tracks = search_spotify_track(access_token, track_name, artist_name)
+
+                output_dict = create_output_dictionary(lastfm_track, spotify_tracks)
+                print("Output Dictionary:")
+                print(json.dumps(output_dict, indent=2))
+            else:
+                print("Track information incomplete.")
+        else:
+            print("No recent Last.fm track found.")
+    else:
+        print("Failed to obtain Spotify access token.")
